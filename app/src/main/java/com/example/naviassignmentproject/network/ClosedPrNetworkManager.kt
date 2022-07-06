e468787b231607b22cdb9589ef828b6e3840e376
parasjotsingh <parasjotsingh@gmail.com>
2022-07-06 20:53:41 +0530

    get closed prs of square okhttp

--- app/src/main/java/com/example/naviassignmentproject/network/ClosedPrNetworkManager.kt
+++ app/src/main/java/com/example/naviassignmentproject/network/ClosedPrNetworkManager.kt
@@ -7,10 +7,12 @@ import retrofit2.Response
 
 object ClosedPrNetworkManager {
     private val closedPrApi = RetrofitHelper.getInstance().create(ClosedPrApi::class.java)
+    private const val ownerName = "square"
+    private const val repoName = "okhttp"
 
     //send request params
     open suspend fun getClosedPrList(): List<ClosedPrModelItem>? {
-        val result: Response<ClosedPrModel> = closedPrApi.getClosedPrData()
+        val result: Response<ClosedPrModel> = closedPrApi.getClosedPrData(ownerName, repoName)
         return if (result.isSuccessful) {
             Log.e("Paras ", result.body().toString())
             result.body()
